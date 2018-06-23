38cbba402e885e0999435e4cc3af1cad7e2e4cb6
jose hernandez <jose2889@gmail.com>
2018-06-23 18:02:04 -0400

    ejemplos de validacion de formularios desde template

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+  .ng-invalid.ng-touched:not(form){
+    border: 1px solid red;
+  }
+  `]
+})
+export class TemplateComponent {
+
+  usuario:object ={
+    Nombre:"jose",
+    Apellido:"hernandez",
+    Correo:""
+  }
+  constructor() { }
+
+
+  guardar(valor:NgForm){
+
+    console.log("VALOR DE LA FORMA ", valor);
+    console.log("VALOR DE LOS CAMPOS: ",valor.value);
+    console.log("VALOR DE USUARIO: ", this.usuario);
+    
+  }
+
+}
